f209571a542eadea7fc1a2e26bc26e4a8d0bbb31
Sito8943 <sito8943@gmail.com>
2023-10-25 05:10:32 -0700

    responsive hook for MDEditor

--- web/src/views/Workspace/components/Legacy/Legacy.jsx
+++ web/src/views/Workspace/components/Legacy/Legacy.jsx
@@ -1,13 +1,18 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useMemo } from "react";
 import { useLocation } from "react-router-dom";
+import { useScreenWidth } from "use-screen-width";
 
 import { parseQueries } from "some-javascript-utils/browser";
 
 import MDEditor from "@uiw/react-md-editor";
+
+// managers
 import { getNote } from "../Note/local";
 
 function Legacy() {
   const location = useLocation();
+
+  const { screenWidth } = useScreenWidth();
 
   const [value, setValue] = useState("");
 
@@ -17,9 +22,16 @@ function Legacy() {
     if (query.note) setValue(getNote(query.note)?.content);
   }, [location]);
 
+  const mobileScreen = useMemo(() => screenWidth < 600, [screenWidth]);
+
   return (
     <div className="w-full h-full pl-5">
-      <MDEditor value={value} onChange={setValue} className="w-full !h-[90%]" />
+      <MDEditor
+        value={value}
+        onChange={setValue}
+        className="w-full !h-[90%]"
+        preview={mobileScreen ? "edit" : "live"}
+      />
     </div>
   );
 }
