ba91b7bb138e8d77ceb4ba9de6823f72649638a6
Sito8943 <sito8943@gmail.com>
2023-12-09 11:46:39 -0800

    Syncing as custom Loaing

---
+++ web/src/components/Syncing/Syncing.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+// @sito/ui
+import { Loading } from "@sito/ui";
+
+function Syncing() {
+  return (
+    <Loading
+      className="sync rounded-full !bg-[#00000000]"
+      strokeWidth="6"
+      color="basics"
+    />
+  );
+}
+
+export default Syncing;
