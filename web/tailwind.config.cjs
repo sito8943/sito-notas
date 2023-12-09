8c44767f85bf93de18bea12934001c518dc674bc
Sito8943 <sito8943@gmail.com>
2023-12-09 06:28:19 -0800

    only basics colors

--- web/tailwind.config.cjs
+++ web/tailwind.config.cjs
@@ -2,10 +2,34 @@
 
 export default {
   // eslint-disable-next-line no-undef
-  presets: [require("./node_modules/@sito/ui/dist/assets/styles/theme")],
 
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   darkMode: "class",
+  theme: {
+    screens: {
+      "2xl": { max: "1535px" },
+      // => @media (max-width: 1535px) { ... }
 
+      xl: { max: "1279px" },
+      // => @media (max-width: 1279px) { ... }
+
+      lg: { max: "1023px" },
+      // => @media (max-width: 1023px) { ... }
+
+      md: { max: "767px" },
+      // => @media (max-width: 767px) { ... }
+
+      sm: { max: "639px" },
+      // => @media (max-width: 639px) { ... }
+
+      xs: { max: "383px" },
+      // => @media (max-width: 383px) { ... }
+    },
+    colors: {
+      dark: { dark: "#101010", default: "#303030", light: "#4e4e4e" },
+      light: { dark: "#e1e1e1", default: "#ededed", light: "#f7f7f7" },
+    },
+    extend: {},
+  },
   plugins: [],
 };
