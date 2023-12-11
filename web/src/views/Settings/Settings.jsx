782307af3aad5ff92ff8715757af1a70dc482646
Sito8943 <sito8943@gmail.com>
2023-12-10 17:46:18 -0800

    fixed padding on Settings

--- web/src/views/Settings/Settings.jsx
+++ web/src/views/Settings/Settings.jsx
@@ -15,7 +15,7 @@ function Settings() {
 
   return (
     <main className="flex flex-col viewport">
-      <div className="p-10 sm:p-3 pt-20 mt-20 flex flex-col gap-10 flex-1">
+      <div className="p-10 sm:p-3 mt-20 !pb-10 flex flex-col gap-5 flex-1">
         <div
           className={`w-10 h-10 fixed bottom-1 left-1 transition-all duration-300 ease-in-out ${
             sync ? "scale-100" : "scale-0"
