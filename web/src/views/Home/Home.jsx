2992a8fd4f1eb058a9632dab16815f6ecb27f1ca
Sito8943 <sito8943@gmail.com>
2024-02-05 09:44:50 -0500

    added toTop in home

--- web/src/views/Home/Home.jsx
+++ web/src/views/Home/Home.jsx
@@ -1,11 +1,16 @@
 import { useState } from "react";
+import { scrollTo } from "some-javascript-utils/browser";
+
+// font awesome
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faChevronUp } from "@fortawesome/free-solid-svg-icons";
 
 // styles
 import "/node_modules/react-grid-layout/css/styles.css";
 import "/node_modules/react-resizable/css/styles.css";
 
 // @sito/ui
-import { Loading } from "@sito/ui";
+import { Loading, Button } from "@sito/ui";
 
 // sections
 import SearchWrapper from "./sections/SearchWrapper";
@@ -17,7 +22,7 @@ function Home() {
 
   return (
     <main className="flex flex-col viewport">
-      <div className="p-10 sm:p-3 mt-20 !pb-10 flex flex-col gap-5 flex-1">
+      <div className="p-10 sm:p-3 mt-20 !pb-5 flex flex-col gap-5 flex-1">
         <h2 className="text-6xl xs:text-4xl -mb-3">Mis notas</h2>
         <SearchWrapper />
 
@@ -31,6 +36,9 @@ function Home() {
           <Loading className="sync rounded-full" strokeWidth="8" />
         </div>
       </div>
+      <Button onClick={() => scrollTo(0, 0)} className="w-full rounded-none">
+        <FontAwesomeIcon icon={faChevronUp} />
+      </Button>
     </main>
   );
 }
