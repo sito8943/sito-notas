370d956486bf596111acef3480ce11d9e37684b8
Sito8943 <sito8943@gmail.com>
2024-07-12 08:18:42 -0400

    updated App

--- web/src/App.jsx
+++ web/src/App.jsx
@@ -3,150 +3,71 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import loadable from "@loadable/component";
 import { useTranslation } from "react-i18next";
 
+// providers
+import { useAccount } from "./providers/AccountProvider";
+
 // layouts
 import Auth from "./layouts/Auth";
-import View from "./layouts/View/View";
+//import View from "./layouts/View/View";
 
 // @sito/ui
-import { Handler, SplashScreen, Notification, useNotification } from "@sito/ui";
-
-// services
-import { refresh, validateUser } from "./services/auth";
-import { fetchAccounts } from "./services/account";
-
-// context
-import { useUser } from "./providers/UserProvider";
-
-// auth cache
-import {
-  cachedUser,
-  getUser,
-  logoutUser,
-  remember,
-  saveUser,
-} from "./utils/auth";
-
-// lang
-import { showError } from "./lang/es";
+import { Handler, SplashScreen, Notification } from "@sito/ui";
 
 // views
 const SignIn = loadable(() => import("./views/Auth/SignIn"));
 const SignOut = loadable(() => import("./views/Auth/SignOut"));
-const Home = loadable(() => import("./views/Home/Home"));
+//const Home = loadable(() => import("./views/Home/Home"));
 const NotFound = loadable(() => import("./views/NotFound/NotFound"));
-const Note = loadable(() => import("./views/Note/Note"));
-const Settings = loadable(() => import("./views/Settings/Settings"));
+//const Note = loadable(() => import("./views/Note/Note"));
+//const Settings = loadable(() => import("./views/Settings/Settings"));
 
 function App() {
+  const [loaded, setLoaded] = useState(true);
   const { t } = useTranslation();
 
-  const { setUserState } = useUser();
-  const { setNotification } = useNotification();
-
-  const [loading, setLoading] = useState(true);
-
-  async function handleUserError(error) {
-    if (error.message === "invalid claim: missing sub claim" && cachedUser()) {
-      const rememberValue = remember();
-      if (rememberValue !== null) {
-        const response = await refresh(getUser().user.email, rememberValue);
-        if (response.error) {
-          handleResponseError(response.error);
-          return;
-        }
-        await handleUserSuccess(response.data);
-      } else {
-        logoutUser();
-      }
-    } else if (cachedUser()) {
-      setUserState({ type: "logged-in", user: getUser(), cached: true });
-    } else {
-      logoutUser();
-    }
-  }
-
-  async function handleUserSuccess(data) {
-    const account = await fetchAccounts({
-      sort: ["updated_at"],
-      user: data.user.id,
-    });
-    if (account.error && account.error !== null) {
-      handleResponseError(account.error);
-      return;
-    }
-    const lastAccount = account.data[0];
-    saveUser({
-      ...getUser(),
-      user: data.user,
-      account: lastAccount,
-    });
-    setUserState({
-      type: "logged-in",
-      user: data.user,
-      account: lastAccount,
-      cached: false,
-    });
-  }
-
-  function handleResponseError(error) {
-    logoutUser();
-    setNotification({
-      type: "error",
-      message: t(`_accessibility:errors.${toCamelCase(error.message)}`),
-    });
-    setLoading(false);
-  }
-
-  async function fetch() {
-    try {
-      const { data, error } = await validateUser();
-      if (error && error !== null) {
-        await handleUserError(error);
-      } else {
-        await handleUserSuccess(data);
-      }
-    } catch (err) {
-      logoutUser();
-      console.error(err);
-    }
-
-    setLoading(false);
-  }
+  const { logUserFromLocal } = useAccount();
 
   useEffect(() => {
-    fetch();
+    document.querySelector("html").style.scrollBehavior = "auto";
+    window.scroll({ top: 0 });
+    document.querySelector("html").style.scrollBehavior = "";
     // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, []);
+  }, [location.pathname]); // triggered on route change
+
+  useEffect(() => {
+    logUserFromLocal();
+    setTimeout(() => {
+      setLoaded(false);
+    }, 1000);
+  }, [logUserFromLocal]);
 
   return (
     <Suspense>
       <Handler>
         <Notification />
         <SplashScreen
-          visible={loading}
+          visible={loaded}
           logo={
             <div>
               <h1 className="uppercase">{t("_accessibility:appName")}</h1>
             </div>
           }
         />
-        {!loading ? (
-          <BrowserRouter>
-            <Routes>
-              <Route exact path="/auth" element={<Auth />}>
-                <Route index element={<SignIn />} />
-              </Route>
-              <Route path="/" element={<View />}>
-                <Route index element={<Home />} />
-                <Route path="/note/:id" element={<Note />} />
-                <Route path="/settings" element={<Settings />} />
-              </Route>
+        <BrowserRouter>
+          <Routes>
+            <Route exact path="/auth" element={<Auth />}>
+              <Route index element={<SignIn />} />
+            </Route>
+            {/*  <Route path="/" element={<View />}>
+              <Route index element={<Home />} />
+              <Route path="/note/:id" element={<Note />} />
+              <Route path="/settings" element={<Settings />} />
+            </Route> */}
 
-              <Route exact path="/sign-out" element={<SignOut />} />
-              <Route path="/*" element={<NotFound />} />
-            </Routes>
-          </BrowserRouter>
-        ) : null}
+            <Route exact path="/sign-out" element={<SignOut />} />
+            <Route path="/*" element={<NotFound />} />
+          </Routes>
+        </BrowserRouter>
       </Handler>
     </Suspense>
   );
