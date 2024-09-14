27877b68ed00abbe1e0d74002f964220f6fd8922
Sito8943 <sito8943@gmail.com>
2024-09-14 11:29:25 -0400

    updated note

--- web/src/views/Note/Note.jsx
+++ web/src/views/Note/Note.jsx
@@ -1,18 +1,19 @@
 import { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
 import { useDebounce } from "use-lodash-debounce";
+import { useQuery, useMutation } from "@tanstack/react-query";
 
 // @sito/ui
 import { InputControl, TextareaControl, useNotification } from "@sito/ui";
 
-// services
-import { fetchNotes, updateNote } from "../../services/notes";
+// providers
+import { useAppApiClient } from "../../providers/AppApiProvider";
 
 // components
 import Syncing from "../../components/Syncing/Syncing";
 
-// providers
-import { useAccount } from "../../providers/AccountProvider";
+// utils
+import { ReactQueryKeys } from "../../utils/queryKeys";
 
 // styles
 import "./styles.css";
@@ -20,105 +21,91 @@ import "./styles.css";
 function Note() {
   const { id } = useParams();
 
-  const [sync, setSync] = useState(false);
-
-  const { userState, setUserState } = useAccount();
+  const appApiClient = useAppApiClient();
   const { setNotification } = useNotification();
-
-  const [note, setNote] = useState({});
-  const [loading, setLoading] = useState(true);
-
-  const localFetchNotes = async (id) => {
-    setLoading(true);
-    if (!userState.notes) {
-      const { data, error } = await fetchNotes();
-      if (error && error !== null) {
-        setNotification({ type: "error", message: error.message });
-        console.error(error.message);
-      }
-      setUserState({ type: "set-notes", notes: data });
-      const found = data.find((note) => note.id === id);
-      if (found) setNote(found);
-    } else {
-      const found = userState.notes.find((note) => note.id === id);
-      if (found) setNote(found);
-    }
-    setLoading(false);
-  };
-
-  useEffect(() => {
-    localFetchNotes(id);
-  }, [id]);
 
   const [toUpdate, setToUpdate] = useState(undefined);
   const debounced = useDebounce(toUpdate, 500);
 
+  const { data, isLoading } = useQuery({
+    queryKey: [ReactQueryKeys.Notes, id],
+    queryFn: () => appApiClient.Note.getById(id),
+    enabled: !!id,
+  });
+
+  const [title, setTitle] = useState(data?.item?.title ?? "");
+  const [content, setContent] = useState(data?.item?.content ?? "");
+
+  const updateNote = useMutation({
+    mutationFn: (note) => appApiClient.Note.update(note),
+    onSuccess: ({ error }) => {
+      if (error) {
+        setNotification({
+          message: error.message,
+          type: "error",
+        });
+        return;
+      }
+    },
+    onError: (error) => {
+      // do something
+      // eslint-disable-next-line no-console
+      console.error(error);
+      setNotification({
+        message: error.message,
+        type: error,
+      });
+    },
+  });
+
   const handleContent = (e) => {
-    setNote({ ...note, content: e.target.value });
-    setToUpdate({ value: e.target.value, attribute: "content" });
+    setContent(e.target.value);
+    setToUpdate({
+      ...data.item,
+      content: e.target.value,
+      last_update: new Date().getMilliseconds(),
+    });
   };
 
   const handleTitle = (e) => {
-    setNote({ ...note, title: e.target.value });
-    setToUpdate({ value: e.target.value, attribute: "title" });
-  };
-
-  const localUpdateNote = async (data) => {
-    setSync(true);
-    const foundIndex = userState.notes.findIndex((noteR) => noteR.id === id);
-    if (foundIndex >= 0) {
-      const now = new Date().getTime();
-      const theNote = userState.notes[foundIndex];
-      const toUpdateDto = { id: theNote.id, last_update: now };
-      theNote.last_update = now;
-      switch (data.attribute) {
-        case "title":
-          toUpdateDto.title = data.value;
-          theNote.title = data.value;
-          break;
-        default:
-          toUpdateDto.content = data.value;
-          theNote.content = data.value;
-          break;
-      }
-      // remote update
-      const error = await updateNote(toUpdateDto);
-      // local update
-      setUserState({ type: "set-notes", notes: [...userState.notes] });
-      if (error && error !== null) console.error(error.message);
-    }
-    setSync(false);
+    setTitle(e.target.value);
+    setToUpdate({
+      ...data.item,
+      title: e.target.value,
+      last_update: new Date().getMilliseconds(),
+    });
   };
 
   useEffect(() => {
-    if (toUpdate) localUpdateNote(toUpdate);
+    if (toUpdate) updateNote.mutate(toUpdate);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [debounced]);
 
   return (
     <main className="flex flex-col viewport">
       <div
         className={`w-10 h-10 fixed bottom-1 left-1 transition-all duration-300 ease-in-out ${
-          sync ? "scale-100" : "scale-0"
+          isLoading || updateNote.isPending ? "scale-100" : "scale-0"
         } pointer-events-none`}
       >
         <Syncing />
       </div>
       <div className="p-10 sm:p-3 mt-20 !pb-10 grid workspace gap-5 flex-1 appear">
-        {loading ? (
+        {isLoading ? (
           <div className="w-full h-[45px] skeleton-box" />
         ) : (
           <InputControl
-            value={note.title ?? ""}
+            value={title}
             onChange={handleTitle}
             className="text-2xl"
           />
         )}
-        {loading ? (
+        {isLoading ? (
           <div className="w-full min-h-[300px] h-full skeleton-box !rounded-xl" />
         ) : (
           <TextareaControl
             label={""}
-            value={note.content ?? ""}
+            value={content}
             onChange={handleContent}
             className="min-h-[300px] h-full"
           />
