f60118a614a11affa820efe39fb8361e0acec1a7
Sito <sito8943@gmail.com>
2025-05-27 12:14:24 -0400

    no grid layout

--- web/src/views/Home/Home.jsx
+++ web/src/views/Home/Home.jsx
@@ -1,23 +1,22 @@
+import { useRef } from "react";
 import { scrollTo } from "some-javascript-utils/browser";
 
 // font awesome
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faChevronUp } from "@fortawesome/free-solid-svg-icons";
 
-// styles
-import "/node_modules/react-grid-layout/css/styles.css";
-import "/node_modules/react-resizable/css/styles.css";
-
 // @sito/ui
 import { Button } from "@sito/ui";
 
 // sections
-import SearchWrapper from "./sections/SearchWrapper";
 import Notes from "./sections/Notes";
+import SearchWrapper from "./sections/SearchWrapper";
 
 function Home() {
+  const mainRef = useRef(null);
+
   return (
-    <main className="flex flex-col h-full overflow-auto">
+    <main ref={mainRef} className="flex flex-col h-full overflow-auto">
       <div className="p-10 sm:p-3 !pb-5 flex flex-col gap-5 flex-1">
         <h2 className="text-6xl xs:text-4xl -mb-3">Mis notas</h2>
         <SearchWrapper />
@@ -25,7 +24,7 @@ function Home() {
       </div>
       <Button
         shape="filled"
-        onClick={() => scrollTo(0, 0)}
+        onClick={() => scrollTo(0, 0, mainRef?.current)}
         className="fixed rounded-full p-6 bottom-20 right-5 w-0 h-0"
       >
         <FontAwesomeIcon icon={faChevronUp} />
